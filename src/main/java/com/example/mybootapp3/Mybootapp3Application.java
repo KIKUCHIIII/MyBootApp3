e1898794274617988fdadd11fcafc28a8f5925e2
hogehoge <hogehoge@gmail.com>
2024-05-21 11:15:36 +0900

    TEST

--- src/main/java/com/example/mybootapp3/Mybootapp3Application.java
+++ src/main/java/com/example/mybootapp3/Mybootapp3Application.java
@@ -3,6 +3,8 @@ package com.example.mybootapp3;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//TEST
+
 @SpringBootApplication
 public class Mybootapp3Application {
 
